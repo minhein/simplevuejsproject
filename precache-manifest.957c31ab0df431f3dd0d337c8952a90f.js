7e01bf0bf51abe59c2d0f254d345f6d8bcdabc93
Min Hein <k.minhein@gmail.com>
2019-10-11 23:59:12 +0100

    deploy

---
+++ precache-manifest.957c31ab0df431f3dd0d337c8952a90f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d3f623d720dcb15d6c19",
+    "url": "/css/app.2e86f4f4.css"
+  },
+  {
+    "revision": "d3f623d720dcb15d6c19",
+    "url": "/js/app.32942c49.js"
+  },
+  {
+    "revision": "769a9f2957a58fdb14db",
+    "url": "/js/chunk-vendors.d34805bd.js"
+  },
+  {
+    "revision": "61bab53bd58b29b218c4e1a3f14c4145",
+    "url": "/media/oh.61bab53b.mp4"
+  },
+  {
+    "revision": "8281bc1d71d550894eb3ee59b386ed5c",
+    "url": "/img/logo.8281bc1d.png"
+  },
+  {
+    "revision": "f4ec98a4be54470e5a9c2fdaebe662db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
